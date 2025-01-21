d1723227b1c6e3c91be1a6aa6a07a1d9dc45747e
Usamscc <usamawaseempixl@gmail.com>
2025-01-21 18:56:43 +0500

    cspotlight texture

---
+++ Assets/_Game/Scripts/Runtime/MoveCoins.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCoins : MonoBehaviour
+{
+   
+
+    void Update()
+    {
+        if (transform.position.x < -10)
+        {
+            
+        } 
+    }
+}
